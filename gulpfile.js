25bf47da18deac7df203b839a083fe6ac512a935
Valeriy <goldaboss@yandex.ru>
2019-06-28 09:00:48 +0300

    app и dist

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,20 @@ gulp.task('styles', function(){ // Создаем таск "sass"
     return gulp.src('app/style.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(gulp.dest('dist/css')) // Выгружаем результата в папку dist/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
 gulp.task('html', function() {
     return gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'))
         .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('scripts', function() {
     return gulp.src('app/script.js')
         .pipe(babel({presets: ['@babel/env']}))
-        .pipe(gulp.dest('app/js'))
+        .pipe(gulp.dest('dist/js')) // Выгружаем результата в папку dist/js
         .pipe(browserSync.reload({ stream: true }))
 
 });
@@ -29,7 +30,7 @@ gulp.task('scripts', function() {
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browser Sync
         server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
+            baseDir: 'dist' // Директория для сервера - dist
         },
         notify: false // Отключаем уведомления
     });
