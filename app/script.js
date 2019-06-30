8e9408e197a0a0f37350363c819b1883dcdcfe67
SashOK <goodfront85>
2019-07-01 00:17:03 +0300

    slider start

---
+++ app/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	//initialize swiper when document ready
+	const mySwiper = new Swiper ('.js-slider', {
+		// Optional parameters
+		slidesPerView: 4,
+		loop: true
+	})
+});
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper('.js-slider', {
+    // Optional parameters
+    slidesPerView: 4,
+    loop: true
+  });
+});
