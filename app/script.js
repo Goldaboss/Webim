a6399783ce678187c933030766a0aec5bf937f05
Valeriy <goldaboss@yandex.ru>
2019-07-01 22:54:44 +0300

    slider

--- app/script.js
+++ app/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
 	//initialize swiper when document ready
-	const mySwiper = new Swiper ('.js-slider', {
+	const clientsSlider = new Swiper ('.js-slider', {
 		// Optional parameters
 		slidesPerView: 4,
+		navigation: {
+			nextEl: '.js-btn-next',
+			prevEl: '.js-btn-prev',
+		},
+		breakpoints: {
+			// when window width is <= 320px
+			576:{
+				slidesPerView: 1,
+			},
+			768: {
+				slidesPerView: 2,
+			},
+			990: {
+				slidesPerView: 3,
+			},
+		},
 		loop: true
 	})
 });
--- dist/js/script.js
+++ dist/js/script.js
@@ -2,9 +2,25 @@
 
 $(document).ready(function () {
   //initialize swiper when document ready
-  var mySwiper = new Swiper('.js-slider', {
+  var clientsSlider = new Swiper('.js-slider', {
     // Optional parameters
     slidesPerView: 4,
+    navigation: {
+      nextEl: '.js-btn-next',
+      prevEl: '.js-btn-prev'
+    },
+    breakpoints: {
+      // when window width is <= 320px
+      576: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      990: {
+        slidesPerView: 3
+      }
+    },
     loop: true
   });
 });
